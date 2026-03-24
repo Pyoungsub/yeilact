681ce34f14dfcc5e83a04df4651b26056ad7e3b4
Pyoungsub <ps.shim87@gmail.com>
2026-03-24 18:47:19 +0900

    hotfix:main video

--- resources/views/components/rectangle-video.blade.php
+++ resources/views/components/rectangle-video.blade.php
@@ -1,6 +1,6 @@
 @props(['source' => 'video/7cf4958d5002916a5141c3b18de475d8.mp4'])
 <div class="">
-    <div class="relative h-full w-full aspect-[9/16] rounded-lg overflow-hidden">
+    <div class="relative h-full w-full aspect-[3/4] rounded-lg overflow-hidden">
         <video class="absolute top-0 left-0 w-full h-full object-cover" playsinline autoplay muted loop>
             <source src="{{ asset('storage/' . $source) }}" type="video/mp4" >
         </video>
--- resources/views/livewire/home/index.blade.php
+++ resources/views/livewire/home/index.blade.php
@@ -1,6 +1,10 @@
 
 <div class="bg-black">
-    <x-mp4 source="/video/7cf4958d5002916a5141c3b18de475d8.mp4" />
+    <div class="relative max-w-2xl aspect-[3/4] rounded-lg overflow-hidden mx-auto">
+        <video class="absolute top-0 left-0 w-full h-full object-cover" playsinline autoplay muted loop>
+            <source src="{{ asset('storage/video/7cf4958d5002916a5141c3b18de475d8.mp4') }}" type="video/mp4">
+        </video>
+    </div>`
     <livewire:courses />
     <livewire:audition />
     <x-promotion />
