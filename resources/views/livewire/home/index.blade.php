631c741ea1bdbc4eb8c1a4a03a5c28768ab7773c
Pyoungsub <ps.shim87@gmail.com>
2026-03-24 19:10:44 +0900

    video size reduced

--- resources/views/livewire/home/index.blade.php
+++ resources/views/livewire/home/index.blade.php
@@ -4,7 +4,7 @@
         <video class="absolute top-0 left-0 w-full h-full object-cover" playsinline autoplay muted loop>
             <source src="{{ asset('storage/video/7cf4958d5002916a5141c3b18de475d8.mp4') }}" type="video/mp4">
         </video>
-    </div>`
+    </div>
     <livewire:courses />
     <livewire:audition />
     <x-promotion />
