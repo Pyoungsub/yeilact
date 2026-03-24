124ee9e6fb145bd65c35952d58c96c4a6bc8bd66
Pyoungsub <ps.shim87@gmail.com>
2026-03-25 08:40:59 +0900

    tuitions

--- resources/views/components/tuitions.blade.php
+++ resources/views/components/tuitions.blade.php
@@ -3,27 +3,27 @@
     x-init="
         swiper = new Swiper($refs.tuitions, {
             slidesPerView: 1.3,
-            spaceBetween: 30,
+            spaceBetween: 15,
             breakpoints: {
                 640: { // sm breakpoint
                     slidesPerView: 1.5,
-                    spaceBetween: 30,
+                    spaceBetween: 15,
                 },
                 768: { // md breakpoint
                     slidesPerView: 2.3,
-                    spaceBetween: 40,
+                    spaceBetween: 15,
                 },
                 1024: { // lg breakpoint
                     slidesPerView: 2.5,
-                    spaceBetween: 50,
+                    spaceBetween: 15,
                 },
                 1280: { // xl breakpoint
                     slidesPerView: 3.3,
-                    spaceBetween: 60,
+                    spaceBetween: 15,
                 },
                 1536: { // 2xl breakpoint
                     slidesPerView: 3.5,
-                    spaceBetween: 70,
+                    spaceBetween: 15,
                 }
             },
         });
@@ -33,9 +33,9 @@
     <div x-ref="tuitions" class="mt-4 w-full overflow-hidden">
         <div class="swiper-wrapper">
             @foreach($tuitions as $tuition)
-                <div class="swiper-slide" wire:key="$tuition->id">
-                    <div class="w-full rounded-xl aspect-square p-4 bg-cover bg-center bg-no-repeat" style="background-image:url({{asset('storage/tuitions/yeilima-b-001.jpg')}})">
-                        {{$tuition}}
+                <div class="swiper-slide" wire:key="tuition-{{ $tuition->id }}">
+                    <div class="w-full rounded-xl aspect-square p-4 bg-cover bg-center bg-no-repeat"
+                        style="background-image:url({{ asset('storage/'.$tuition->img_path) }})">
                     </div>
                 </div>
             @endforeach
