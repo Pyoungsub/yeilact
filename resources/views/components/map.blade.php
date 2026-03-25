1bac14bc79845dffd80857317a3dcece06403d11
Pyoungsub <ps.shim87@gmail.com>
2026-03-25 15:47:12 +0900

    map added

--- resources/views/components/map.blade.php
+++ resources/views/components/map.blade.php
@@ -1,25 +1,29 @@
 <div class="mt-12 max-w-5xl mx-auto px-2">
-    <div class="">
+    <div>
         <h1 class="text-2xl font-bold">오시는 길</h1>
         <p>서울특별시 양천구 목동로 213</p>
     </div>
-    <div id="map" class="my-4 rounded-lg aspect-video w-full flex items-center justify-center border shadow-lg"></div>
-    <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=fe9dd7241ae66dbc0aa163efbca15817"></script>
+
+    <div id="map" class="my-4 rounded-lg aspect-video w-full border shadow-lg"></div>
+
     <script>
-        var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
-    mapOption = { 
-        center: new kakao.maps.LatLng(37.52759681049792, 126.86368533824552), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
-    };
+        function initMap() {
 
-var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+            const location = { lat: 37.52759681049792, lng: 126.86368533824552 };
 
-// 지도를 클릭한 위치에 표출할 마커입니다
-var marker = new kakao.maps.Marker({ 
-    // 지도 중심좌표에 마커를 생성합니다 
-    position: map.getCenter() 
-}); 
-// 지도에 마커를 표시합니다
-marker.setMap(map);
+            const map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 16,
+                center: location,
+            });
+
+            const marker = new google.maps.Marker({
+                position: location,
+                map: map,
+            });
+        }
+    </script>
+
+    <script async
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDAlCgmo1pX_ZgicCA-qG_Yh7o1notDwgo&callback=initMap">
     </script>
 </div>
